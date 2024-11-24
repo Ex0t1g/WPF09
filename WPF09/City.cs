2a4ad05f9e4ee4776448be0114103692dba4777e
Ex0t1g Ex0t1g <ex0t1g@mail.ru>
2024-11-24 23:13:10 +0300

    Добавьте файлы проекта.

---
+++ WPF09/City.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace WPF09
+{
+    public class City
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ WPF09/MainViewModel.cs
@@ -0,0 +1,76 @@
+﻿
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WPF09
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<City> _cities;
+        private City _selectedCity;
+        public ICommand AddCityCommand { get; }
+        public ICommand DeleteCityCommand { get; }
+        public ObservableCollection<Country> Countries { get; set; }
+        public ObservableCollection<City> Cities
+        {
+            get => _cities;
+            set
+            {
+                _cities = value;
+                OnPropertyChanged(nameof(Cities));
+            }
+        }
+        public City SelectedCity
+        {
+            get => _selectedCity;
+            set
+            {
+                _selectedCity = value;
+                OnPropertyChanged(nameof(SelectedCity));
+            }
+        }
+        public MainViewModel()
+        {
+            Cities = new ObservableCollection<City>
+            {
+                new City { ID = 1, Name = "Москва", Country = "Россия" },
+                new City { ID = 2, Name = "Нью-Йорк", Country = "США" }
+            };
+
+            Countries = new ObservableCollection<Country>
+            {
+                new Country { Name = "Россия" },
+                new Country { Name = "США" },
+                new Country { Name = "Канада" }
+            };
+
+            AddCityCommand = new RelayCommand(AddCity);
+            DeleteCityCommand = new RelayCommand(DeleteCity, CanDeleteCity);
+        }
+        private void AddCity(object parameter)
+        {
+            var newId = Cities.Any() ? Cities.Max(c => c.ID) + 1 : 1; 
+            Cities.Add(new City { ID = newId, Name = "Новый Город", Country = "Россия" }); 
+        }
+
+        private bool CanDeleteCity(object parameter)
+        {
+            return SelectedCity != null; 
+        }
+
+        private void DeleteCity(object parameter)
+        {
+            if (SelectedCity != null)
+            {
+                Cities.Remove(SelectedCity);
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
